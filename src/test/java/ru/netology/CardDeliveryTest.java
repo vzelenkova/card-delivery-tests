4cc3e00402e3409f8e8c80ecf96cc7736ff1d159
vzelenkova <95612348+vzelenkova@users.noreply.github.com>
2025-04-04 14:10:48 +0300

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,10 +1,10 @@
 package ru.netology;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
+import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
@@ -41,7 +41,7 @@ public class CardDeliveryTest {
         $("[name='agreement']").click();
 
         // Нажать кнопку "Забронировать"
-        $("button[type='button']").filter(text("Забронировать")).click();
+        $$("button[type='button']").findBy(text("Забронировать")).click();
 
         // Проверка успешной отправки формы
         $("div[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(20));
