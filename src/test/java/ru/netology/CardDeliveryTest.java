26b7bbb32468b08ba6d7a85f1a2fbc9bd994b903
vzelenkova <95612348+vzelenkova@users.noreply.github.com>
2025-04-04 17:03:04 +0300

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,48 +1,54 @@
 package ru.netology;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import org.openqa.selenium.By;
 
 public class CardDeliveryTest {
 
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "http://localhost:9999";
-        Configuration.holdBrowserOpen = false;
-        Configuration.headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "true"));
     }
 
-   @Test
-void shouldSubmitFormSuccessfully() {
-    
-    open("/");
+    @Test
+    void shouldSubmitFormSuccessfully() {
+        open("/");
 
-    $("form").shouldBe(visible);
+        // Формируем дату на 3 дня вперёд
+        String futureDate = LocalDate.now().plusDays(3)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-    String futureDate = LocalDate.now().plusDays(3)
-            .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        // Заполняем поля формы
+        $("[data-test-id='city'] input").shouldBe(visible).setValue("Москва");
 
-    $("[data-test-id='city'] input").shouldBe(visible).setValue("Москва"); 
-    $("[data-test-id='date'] input").shouldBe(visible).setValue(futureDate); 
-    $("[data-test-id='name'] input").shouldBe(visible).setValue("Иван Иванов"); 
-    $("[data-test-id='phone'] input").shouldBe(visible).setValue("+79991234567"); 
+        SelenideElement dateInput = $("[data-test-id='date'] input");
+        dateInput.shouldBe(visible).doubleClick().sendKeys(Keys.BACK_SPACE);
+        dateInput.setValue(futureDate);
 
-    $("[data-test-id='agreement']").shouldBe(visible).click(); 
-    $("[name='agreement']").shouldBe(selected); 
+        $("[data-test-id='name'] input").shouldBe(visible).setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").shouldBe(visible).setValue("+79991234567");
 
-    $$("button").findBy(text("Забронировать"))
-            .shouldBe(visible, Duration.ofSeconds(10)) 
-            .click();
+        $("[data-test-id='agreement']").shouldBe(visible).click();
+        $("[name='agreement']").shouldBe(selected);
 
-    $("div[data-test-id='notification']")
-            .shouldBe(visible, Duration.ofSeconds(20));
-}
+        // Отправляем форму
+        $$("button").findBy(text("Забронировать"))
+                .shouldBe(visible, Duration.ofSeconds(10))
+                .click();
+
+        // Проверка результата
+        $("[data-test-id='notification']")
+                .shouldBe(visible, Duration.ofSeconds(20))
+                .shouldHave(text("Встреча успешно запланирована на " + futureDate));
+    }
 }
